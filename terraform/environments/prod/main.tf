687852f747b8edc7fd514abdbd64407fbb99efc4
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-06 23:57:50 +0100

    Change EC2 instance type from t3.micro to t3.medium

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -13,7 +13,7 @@ module "ec2" {
 
   name             = "ikenna-web-prod-ec2"
   ami = data.aws_ami.ubuntu.id 
-  instance_type    = "t3.micro"
+  instance_type    = "t3.medium"
   vpc_id           = data.aws_vpc.main.id
   private_subnets  = data.aws_subnets.private.ids
 
