3e248d27d0cc2b704efb23bf08b55a52944ce604
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-08 23:12:07 +0100

    Add EC2 instance resource for FastAPI server

---
+++ terraform/environments/prod/ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "fastapi" {
+
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.medium"
+
+  subnet_id                   = aws_subnet.public_1.id
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  associate_public_ip_address = true
+
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
+
+  tags = {
+    Name = "fastapi-server"
+  }
+}
