daa16c80224f30846122070a0fce44cd161bb7a8
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-08 15:40:30 +0100

    Create iam.tf

---
+++ terraform/bootstrap/iam.tf
@@ -0,0 +1,45 @@
+data "aws_iam_policy_document" "github_oidc_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+
+      identifiers = [
+        aws_iam_openid_connect_provider.github.arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
+        "repo:${var.github_owner}/${var.github_repository}:ref:refs/heads/${var.github_branch}"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "administrator_access" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
