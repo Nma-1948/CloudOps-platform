520195ca77384292547d21100a80199c03537ae9
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-08 16:21:08 +0100

    Create S3 bucket and DynamoDB table for Terraform
    Added S3 bucket, versioning, encryption, and DynamoDB lock table resources.

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,39 @@
+# S3 bucket
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "aws-private-ec2-tf-state-786827631657"
+}
+
+# Versioning (PUT YOUR CODE HERE)
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
