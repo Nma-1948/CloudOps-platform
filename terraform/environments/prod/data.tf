7d169d84257e40fa57fb95f9e92c0be81998042b
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-08 16:46:20 +0100

    Remove VPC and subnet data sources from data.tf
    Removed data sources for existing VPC and subnets.

--- terraform/environments/prod/data.tf
+++ terraform/environments/prod/data.tf
@@ -1,34 +1,4 @@
-# Get existing VPC
-data "aws_vpc" "main" {
-  filter {
-    name   = "tag:Name"
-    values = ["JimmyTechVPC"]
-  }
-}
 
-# Get public subnets
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main.id]
-  }
-
-  tags = {
-    Name = "Public-*"
-  }
-}
-
-# Get private subnets
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main.id]
-  }
-
-  tags = {
-    Name = "Private-*"
-  }
-}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
