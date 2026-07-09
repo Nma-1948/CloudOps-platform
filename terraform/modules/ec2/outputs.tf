19bdf3f29765f296e67235a8c8068d480d3f6385
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-10 00:52:32 +0100

    Add public and private IP outputs for EC2 instance

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -2,6 +2,10 @@ output "instance_id" {
   value = aws_instance.app.id
 }
 
-output "ec2_sg_id" {
-  value = data.aws_security_group.ec2_sg.id
+output "public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.app.private_ip
 }
