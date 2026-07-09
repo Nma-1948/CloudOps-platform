b4a91af719965252e349601e4439a4ecc7794e64
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-09 15:57:06 +0100

    Add S3 backend configuration for Terraform

---
+++ terraform/bootstrap/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "nma1948-terraform-state-116101834005"
+    key            = "prod/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
