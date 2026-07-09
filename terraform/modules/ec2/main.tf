ff13b16dd6f4238d8b6caf1d83d909562feac055
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-10 00:28:30 +0100

    Refactor EC2 instance configuration in main.tf
    Updated EC2 instance configuration to use a variable for subnet ID and security group ID. Changed public IP association to true and modified user data script for nginx installation.

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,22 +1,23 @@
-data "aws_security_group" "ec2_sg" {
-  name   = "my-app-sg"
-  vpc_id = var.vpc_id
-}
 
 resource "aws_instance" "app" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = var.private_subnets[0]
-  vpc_security_group_ids = [data.aws_security_group.ec2_sg.id]
-  associate_public_ip_address = false
+  ami                         = var.ami
+  instance_type               = var.instance_type
+
+  subnet_id                   = var.subnet_id
+
+  vpc_security_group_ids = [
+    var.security_group_id
+  ]
+
+  associate_public_ip_address = true
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
+#!/bin/bash
+apt update -y
+apt install -y nginx
+systemctl enable nginx
+systemctl start nginx
+EOF
 
   tags = {
     Name = var.name
@@ -28,5 +29,4 @@ resource "time_sleep" "wait_for_instance" {
 
   create_duration = "30s"
 }
-
 
