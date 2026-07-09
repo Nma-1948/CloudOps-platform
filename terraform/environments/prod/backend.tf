b2b4aa1eec611ba5955f526bb4d73815a5ca25f4
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-09 14:38:06 +0100

    Enhance S3 backend configuration with locking and encryption
    Added DynamoDB table for state locking and enabled encryption.

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,5 +1,9 @@
-backend "s3" {
-  bucket = "nma1948-terraform-state-116101834005"
-  key    = "prod/terraform.tfstate"
-  region = "eu-west-1"
-}
+terraform {
+‎  backend "s3" {
+‎    bucket ="nma1948-terraform-state-116101834005"
+‎    key            = "prod/terraform.tfstate"
+‎    region         = "eu-west-1"
+‎    dynamodb_table = "terraform-lock"
+‎    encrypt        = true
+‎  }
+‎}
