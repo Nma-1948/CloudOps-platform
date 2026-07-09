2dc1c3099aa4b7432eb08dbd947592486d2833f6
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-09 11:37:14 +0100

    Change S3 bucket for Terraform backend
    Updated S3 backend configuration for Terraform.

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,8 +1,5 @@
-terraform {
-  backend "s3" {
-    bucket         = "ikenna-terraform-state"   # 🔁 choose a unique name
-    key            = "prod/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-locks"
-  }
+backend "s3" {
+  bucket = "nma1948-terraform-state-116101834005"
+  key    = "prod/terraform.tfstate"
+  region = "eu-west-1"
 }
