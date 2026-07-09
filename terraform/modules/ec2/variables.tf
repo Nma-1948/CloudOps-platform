857cabd5f5b952ef5de75279dd4c79e7673452cf
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-10 00:26:15 +0100

    Update variables.tf

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,19 +1,25 @@
-variable "vpc_id" {}
-
-variable "private_subnets" {
-  type = list(string)
+variable "vpc_id" {
+  type = string
 }
 
+variable "subnet_id" {
+  type = string
+}
 
+variable "security_group_id" {
+  type = string
+}
 
 variable "ami" {
-  default = "ami-09d0c9a85bf1b9ea7" # Ubuntu (your region)
+  type = string
 }
 
 variable "instance_type" {
+  type    = string
   default = "t3.medium"
 }
 
 variable "name" {
+  type    = string
   default = "fastapi-server"
 }
