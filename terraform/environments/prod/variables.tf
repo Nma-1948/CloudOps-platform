b637819dcb9553f74e0645b55c5a274c47568cdf
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-10 09:15:39 +0100

    Fix description casing for alert_email variable
    Updated description for alert_email variable to use 'CloudWatch' instead of 'cloudwatch'.

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
-  default = "eu-west-1"
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
 }
 
 variable "alert_email" {
-  description = "Email address for cloudwatch alerts"
+  description = "Email address for CloudWatch alerts"
   type        = string
 }
 
