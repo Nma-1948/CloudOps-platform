19308e13845b24bebd22f914e81a882b955f3567
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-10 16:46:29 +0100

    Add CloudWatch agent policy attachment to IAM role

--- terraform/environments/prod/iam_ssm.tf
+++ terraform/environments/prod/iam_ssm.tf
@@ -24,3 +24,8 @@ resource "aws_iam_instance_profile" "ec2" {
   name = "ec2-ssm-profile"
   role = aws_iam_role.ec2_ssm_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
