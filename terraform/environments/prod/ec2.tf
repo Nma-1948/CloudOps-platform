87de8b374230d60fdc4778c22ae9f2e2a7a25004
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-10 07:50:44 +0100

    Change EC2 instance type from t3.medium to t3.micro

--- terraform/environments/prod/ec2.tf
+++ terraform/environments/prod/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "fastapi" {
 
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.medium"
+  instance_type               = "t3.micro"
 
   subnet_id                   = aws_subnet.public_1.id
 
