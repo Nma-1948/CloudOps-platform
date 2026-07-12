7ab0bc53467c067da8e04b0f1a115c1283b19c16
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-12 10:06:59 +0100

    Reorder outputs in outputs.tf file

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -1,9 +1,9 @@
-output "public_ip" {
-  value = module.ec2.public_ip
-}
-
 output "instance_id" {
   value = module.ec2.instance_id
+}
+
+output "public_ip" {
+  value = module.ec2.public_ip
 }
 
 output "private_ip" {
