07a380594a876d03255abcfcefeddc47b53eea0f
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-12 09:33:31 +0100

    Update EC2 outputs to include Elastic IP details

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -3,9 +3,13 @@ output "instance_id" {
 }
 
 output "public_ip" {
-  value = aws_instance.app.public_ip
+  value = aws_eip.app.public_ip
 }
 
 output "private_ip" {
   value = aws_instance.app.private_ip
+}
+
+output "elastic_ip_allocation_id" {
+  value = aws_eip.app.id
 }
