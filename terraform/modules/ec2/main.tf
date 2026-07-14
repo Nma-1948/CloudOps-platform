4a69a9f7b443f267af1d65fe5d1a854dc23573cd
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-14 23:56:27 +0100

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,33 +1,15 @@
 resource "aws_instance" "app" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.security_group_id]
-  associate_public_ip_address = true
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnets[0]
+  vpc_security_group_ids = [data.aws_security_group.ec2_sg.id]
 
-  user_data = <<-EOF
-#!/bin/bash
-set -eux
-
-# Update packages
-apt-get update -y
-
-# Install Nginx
-apt-get install -y nginx
-systemctl enable nginx
-systemctl start nginx
-
-# Install CloudWatch Agent
-wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-dpkg -i amazon-cloudwatch-agent.deb
-
-# Start CloudWatch Agent using SSM Parameter Store
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-  -a fetch-config \
-  -m ec2 \
-  -c ssm:/amazon-cloudwatch-agent/config \
-  -s
-EOF
+  user_data = templatefile(
+    "${path.module}/templates/cloud-init.yaml",
+    {
+      hostname = var.name
+    }
+  )
 
   tags = {
     Name = var.name
@@ -44,8 +26,4 @@ resource "aws_eip" "app" {
   }
 }
 
-resource "time_sleep" "wait_for_instance" {
-  depends_on = [aws_instance.app]
 
-  create_duration = "30s"
-}
