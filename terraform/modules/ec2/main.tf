f0ed36c9f88bde78d19ea2b4bf18eaa49e8d6b6a
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-15 07:49:22 +0100

    Enhance EC2 user data with additional scripts
    Add scripts for Docker, k3s, and Helm to user data.

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -8,7 +8,11 @@ resource "aws_instance" "app" {
 
   user_data = templatefile(
     "${path.module}/cloud-init/cloud-init.yaml",
-    {}
+    {
+      docker_script = file("${path.module}/cloud-init/docker.sh")
+      k3s_script    = file("${path.module}/cloud-init/k3s.sh")
+      helm_script   = file("${path.module}/cloud-init/helm.sh")
+    }
   )
 
   tags = {
