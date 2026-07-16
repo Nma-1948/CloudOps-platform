3d069bd600ebd728d0280802f413840d1d8e1c0d
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-16 23:14:25 +0100

    Format docker_script for better readability

--- terraform/modules/ec2/cloud-init.tf
+++ terraform/modules/ec2/cloud-init.tf
@@ -9,7 +9,11 @@ data "cloudinit_config" "app" {
     content = templatefile(
       "${path.module}/cloud-init/cloud-init.yaml.tpl",
       {
-        docker_script = file("${path.module}/cloud-init/docker.sh")
+        docker_script = replace(
+  "      ${file("${path.module}/cloud-init/docker.sh")}",
+  "\n",
+  "\n      "
+)
       }
     )
   }
