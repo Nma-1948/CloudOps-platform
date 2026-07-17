ae1923712d0f0083a1aafc78112a26dc624d7d1d
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-17 07:58:43 +0100

    Add variables for EC2 module in Terraform

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,28 @@
+variable "name" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "instance_profile" {
+  type = string
+}
+
+variable "associate_public_ip" {
+  type    = bool
+  default = false
+}
