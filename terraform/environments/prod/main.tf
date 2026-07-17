aa1dd24726aa2334928954cc1c50bc32e1c46cc0
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-17 08:16:08 +0100

    Change EC2 instance type to t3.micro

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -3,7 +3,7 @@ module "ec2" {
 
   name                 = "fastapi-server"
   ami                  = data.aws_ami.ubuntu.id
-  instance_type        = var.instance_type
+  instance_type        = "t3.micro"
   subnet_id            = aws_subnet.public_1.id
   security_group_id    = aws_security_group.ec2_sg.id
   iam_instance_profile = aws_iam_instance_profile.ec2.name
