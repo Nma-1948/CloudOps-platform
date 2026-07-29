09bcf5ad4dc56bfec6a1333e7bfdfe78eeba1a2b
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-29 23:50:15 +0100

    Add IAM role for EC2 with assume role policy

---
+++ terraform/iam_ssm.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "ec2" {
+  name = "${local.prefix}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
