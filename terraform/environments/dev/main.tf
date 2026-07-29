954c18b16c4670dc659032d1d531134c41ee756f
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-29 09:03:11 +0100

    Create main.tf

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,11 @@
+module "ec2" {
+  source = "../../modules/ec2"
+
+  name                 = "cloudops-dev-server"
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = "t3.small"
+  subnet_id            = aws_subnet.public_1.id
+  security_group_id    = aws_security_group.ec2_sg.id
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
+  associate_public_ip  = true
+}
