cfd99d36c0211cf92ae258345366d749e3ae587f
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-29 17:51:21 +0100

    Refactor EC2 module name to use local prefix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
 module "ec2" {
   source = "./modules/ec2"
 
-  name                 = "${var.project_name}-${var.environment}-${var.server_name}"
+  name                 = "${local.prefix}-${var.server_name}"
   ami                  = data.aws_ami.ubuntu.id
   instance_type        = var.instance_type
   subnet_id            = aws_subnet.public_1.id
   security_group_id    = aws_security_group.ec2_sg.id
   iam_instance_profile = aws_iam_instance_profile.ec2.name
   associate_public_ip  = true
+
+  tags = local.common_tags
 }
