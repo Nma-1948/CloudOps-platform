5ab294ddb396ae6be4a83c0b34674049e9f80047
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-29 23:36:46 +0100

    Add ingress and egress rules for security group

--- terraform/security.tf
+++ terraform/security.tf
@@ -10,3 +10,59 @@ resource "aws_security_group" "ec2_sg" {
     }
   )
 }
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  description = "Allow HTTP"
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  description = "Allow HTTPS"
+
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "backend" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  description = "Backend API"
+
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "k3s" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  description = "K3s API"
+
+  from_port   = 6443
+  to_port     = 6443
+  ip_protocol = "tcp"
+
+  cidr_ipv4 = "10.0.0.0/16"
+}
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.ec2_sg.id
+
+  ip_protocol = "-1"
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
