4448177a3585599fa0c01e1ed012b4725520a065
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-31 00:21:54 +0100

    Refactor route table resource for public subnet association
    Replaced aws_route_table resource with aws_route_ociation for public_1.

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -52,23 +52,7 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.prefix}-public-rt"
-    }
-  )
-}
-
-resource "aws_route_table_association" "public_1" {
+resource "aws_route_ociation" "public_1" {
   subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
 }
@@ -77,3 +61,4 @@ resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
+
