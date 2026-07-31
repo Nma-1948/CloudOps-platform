2fb03aa8c5e62ade1b2b22e613936cd8257b81eb
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-31 01:34:26 +0100

    Refactor EC2 outputs to use 'this' instance
    Updated output references to use 'this' instead of 'app' for instance attributes.

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,11 +1,19 @@
 output "instance_id" {
-  value = aws_instance.app.id
-}
-
-output "private_ip" {
-  value = aws_instance.app.private_ip
+  value = aws_instance.this.id
 }
 
 output "public_ip" {
-  value = aws_instance.app.public_ip
+  value = aws_instance.this.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.this.public_dns
+}
+
+output "private_ip" {
+  value = aws_instance.this.private_ip
+}
+
+output "availability_zone" {
+  value = aws_instance.this.availability_zone
 }
