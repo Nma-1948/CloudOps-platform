960e98c425aa8732efa64c7b51b1eea869c7a0b2
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-31 14:51:22 +0100

    Update required Terraform version and default tags

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.6"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
@@ -13,11 +13,6 @@ provider "aws" {
   region = var.region
 
   default_tags {
-    tags = {
-      Project     = var.project_name
-      Environment = var.environment
-      ManagedBy   = "Terraform"
-      Owner       = "Nma-1948"
-    }
+    tags = local.common_tags
   }
 }
