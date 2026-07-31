b3dcbffbfbe0ca12eb79edff78cfdaa30b98077d
Nma-1948 <glorianmandumele1948@gmail.com>
2026-07-31 14:07:58 +0100

    Fix formatting for security group and IAM profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ module "ec2" {
   ami                  = data.aws_ami.ubuntu.id
   instance_type        = var.instance_type
   subnet_id            = module.network.public_subnet_1_id
-security_group_id    = module.security.ec2_security_group_id
-iam_instance_profile = module.iam.instance_profile_name
+  security_group_id    = module.security.ec2_security_group_id
+  iam_instance_profile = module.iam.instance_profile_name
   associate_public_ip  = true
 
   user_data = templatefile("${path.module}/scripts/user_data.sh.tpl", {
