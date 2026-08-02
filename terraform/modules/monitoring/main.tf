c3053596ab3b91ddce4e6a9a6ee215ac5275419d
Nma-1948 <glorianmandumele1948@gmail.com>
2026-08-02 14:34:01 +0100

    Delete cloudwatch agent SSM parameter resource
    Remove aws_ssm_parameter resource for cloudwatch agent configuration.

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,11 +1,1 @@
 
-
-resource "aws_ssm_parameter" "cloudwatch_agent_config" {
-  name = "/${var.project_name}/${var.environment}/cloudwatch-agent/config"
-  type = "String"
-
-  value = var.cloudwatch_agent_config
-
-  tags = var.tags
-}
-
