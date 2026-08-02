478b5ef89e6a26c458e913cb5056a960bd0cbbd8
Nma-1948 <glorianmandumele1948@gmail.com>
2026-08-02 18:16:39 +0100

    Fix formatting and whitespace in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source       = "./modules/network"
+  source = "./modules/network"
 
   project_name = var.project_name
   environment  = var.environment
@@ -8,7 +8,7 @@ module "network" {
 }
 
 module "security" {
-  source       = "./modules/security"
+  source = "./modules/security"
 
   project_name = var.project_name
   environment  = var.environment
@@ -19,7 +19,7 @@ module "security" {
 }
 
 module "iam" {
-  source       = "./modules/iam"
+  source = "./modules/iam"
 
   project_name = var.project_name
   environment  = var.environment
@@ -28,16 +28,14 @@ module "iam" {
 }
 
 module "ec2" {
-  source               = "./modules/ec2"
+  source = "./modules/ec2"
 
   name                 = "${local.prefix}-${var.server_name}"
   ami                  = data.aws_ami.ubuntu.id
   instance_type        = var.instance_type
-
   subnet_id            = module.network.public_subnet_1_id
   security_group_id    = module.security.ec2_security_group_id
   iam_instance_profile = module.iam.instance_profile_name
-
   associate_public_ip  = true
 
   user_data = templatefile("${path.module}/scripts/user_data.sh.tpl", {
@@ -49,21 +47,21 @@ module "ec2" {
 }
 
 module "monitoring" {
-  source       = "./modules/monitoring"
+  source = "./modules/monitoring"
 
   project_name = var.project_name
   environment  = var.environment
 
-  instance_id  = module.ec2.instance_id
+  instance_id = module.ec2.instance_id
 
-  alert_email  = var.alert_email
+  alert_email = var.alert_email
 
   cloudwatch_agent_config = jsonencode({
     agent = {
       metrics_collection_interval = 60
     }
 
-    metrics     = {
+    metrics = {
       namespace = "${var.project_name}/${var.environment}"
     }
   })
